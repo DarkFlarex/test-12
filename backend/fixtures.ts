42c7be7bff99f068110aac7be1c5d0f858397d4a
DarkFlarex <darkflarexxx@gmail.com>
2024-10-19 20:04:48 +0600

    Добавили фикстуры

--- backend/fixtures.ts
+++ backend/fixtures.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import config from './config';
 
 import User from "./models/User";
+import Photo from "./models/Photo";
 
 const run = async () => {
   await mongoose.connect(config.database);
@@ -34,6 +35,38 @@ const run = async () => {
   await user1.save();
 
   await user2.save();
+
+  const photo = await Photo.create([
+    {
+      user: user1,
+      title: 'Курт Кобейн',
+      image: 'fixtures/kurt.jpg',
+    },
+    {
+      user: user1,
+      title: 'Леди Гага',
+      image: 'fixtures/gaga.jpg',
+    },
+    {
+      user: user1,
+      title: 'The Weeknd',
+      image: 'fixtures/The_Weeknd.png',
+    },
+    {
+      user: user2,
+      title: 'Пингвин',
+      image: 'fixtures/terrifiednootnoot.webp',
+    },  {
+      user: user2,
+      title: 'Гай Юлий Цезарь',
+      image: 'fixtures/cac.jpg',
+    },
+    {
+      user: user2,
+      title: 'Цирилла',
+      image: 'fixtures/cirila.jpg',
+    },
+  ]);
 
   await db.close();
 };
