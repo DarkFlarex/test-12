43a9db798e31b42bea6509ac31dc1ed0e8c43882
DarkFlarex <darkflarexxx@gmail.com>
2024-10-19 13:33:50 +0600

    Создаем модель Photo. Добавили get запрос по query,get по id, post , delete

---
+++ backend/models/Photo.ts
@@ -0,0 +1,32 @@
+import mongoose, {Types} from "mongoose";
+import User from "./User";
+
+
+const Schema = mongoose.Schema;
+
+const PhotoSchema = new Schema({
+    user:{
+        type:Schema.Types.ObjectId,
+        ref: 'User',
+        required:true,
+        validate:{
+            validator: async (value: Types.ObjectId) =>{
+                const createUser = await User.findById(value);
+                return Boolean(createUser);
+            },
+            message: 'User does not exist',
+        }
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+});
+
+const Photo = mongoose.model('Photo', PhotoSchema);
+
+export default Photo;
---
+++ backend/routers/photos.ts
@@ -0,0 +1,79 @@
+import express from "express";
+import mongoose from "mongoose";
+import {imagesUpload} from "../multer";
+import auth, {RequestWithUser} from "../middleware/auth";
+import Photo from "../models/Photo";
+
+const photosRouter = express.Router();
+
+photosRouter.get('/', async (req: RequestWithUser, res, next) => {
+    try {
+        const filter: Record<string, unknown> = {};
+
+        if (req.query.user) {
+            filter.user = req.query.user;
+        }
+
+        const photos = await Photo.find();
+        return res.send(photos);
+    } catch (error) {
+        next(error);
+    }
+});
+
+photosRouter.get('/:id', async (req: RequestWithUser, res, next) => {
+    try {
+        const photo = await Photo.findById(req.params.id);
+        if (!photo) {
+            return res.status(404).send({ error: 'Photo not found' });
+        }
+        return res.send(photo);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+photosRouter.post('/', auth, imagesUpload.single("image"), async (req: RequestWithUser, res, next) => {
+    try {
+        if (!req.user) {
+            return res.status(401).send({ error: 'User not authenticated' });
+        }
+
+        const photo = await Photo.create({
+            user: req.user._id,
+            title: req.body.title,
+            image: req.file?.filename,
+        });
+        return res.send(photo);
+    } catch (error) {
+        if (error instanceof mongoose.Error.ValidationError) {
+            return res.status(400).send(error);
+        }
+        return next(error);
+    }
+});
+
+photosRouter.delete('/:id', auth, async (req, res, next) => {
+    try {
+        const user = (req as RequestWithUser).user;
+
+        const photo = await Photo.findOne({_id: req.params.id});
+
+        if (!photo) {
+            return res.status(404).send({error: 'Not found'});
+        }
+
+        if (photo.user.toString() === user?._id.toString() || user?.role === 'admin') {
+            await photo.deleteOne({_id: req.params.id});
+        } else {
+            return res.status(403).send({error: 'Unauthorized'});
+        }
+
+        return res.status(204).send();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+export default photosRouter;
